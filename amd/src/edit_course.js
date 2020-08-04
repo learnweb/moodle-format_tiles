d074f659e98bb3d1dd1a64ec07672be1a5debb6c
David Watson <david@evolutioncode.uk>
2020-08-04 16:38:18 +0100

    Issue #39 avoid conflict with core usage of data-sectionid

--- amd/src/edit_course.js
+++ amd/src/edit_course.js
@@ -171,10 +171,10 @@ define(
                     }
 
                     // We don't want to re-implement show/hide sections, so we let core handle it.
-                    // Core will re-render all activties when it hides section, and they will be in non subtiles form.
+                    // Core will re-render all activities when it hides section, and they will be in non subtiles form.
                     // So we just delete them and can add them back when we un-hide or expand.
                     $('body').on('click keypress', Selector.SECTION_MAIN + ' ' +
-                        Selector.SECTIONACTIONMENU + '[data-sectionid] ' +
+                        Selector.SECTIONACTIONMENU + '[data-true-sectionid] ' +
                         'a[data-action]', function(e) {
                             var target = $(e.target).closest(Selector.MENU_ACTION);
                             var sectionMain = target.closest(Selector.SECTION_MAIN);
