098c9658f0717d9ed2e69199e648354fbead0f05
David Watson <david@evolutioncode.uk>
2020-11-15 10:28:19 +0000

    Version 3.9.0.9 (2020080609)

--- version.php
+++ version.php
@@ -24,8 +24,8 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2020080608;        // The current plugin version (Date: YYYYMMDDXX).
+$plugin->version   = 2020080609;        // The current plugin version (Date: YYYYMMDDXX).
 $plugin->requires  = 2019111200;        // Requires this Moodle version.
 $plugin->component = 'format_tiles';    // Full name of the plugin (used for diagnostics).
 $plugin->maturity  = MATURITY_STABLE;
-$plugin->release = "3.9.0.8";
+$plugin->release = "3.9.0.9";
